216f778e1935442ab5b0edb797204787118cb253
tatuya4451 <tatuya44511336@gmail.com>
2021-01-29 23:58:13 +0900

    カレンダープログラムを修正

--- 02.calendar/calender.rb
+++ 02.calendar/calender.rb
@@ -3,41 +3,32 @@
 require 'date'
 require 'optparse'
 
-def create_calender(month: Date.today.month, year: Date.today.year)
-  i = 3
-  first_day = Date.new(year, month, 1)
-  first_wday = Date.new(year, month, 1).wday
-  last_day = Date.new(year, month, -1)
-  week = ["日", "月", "火", "水", "木","金","土"]
+def create_calender(month, year)
+  width = 3
+  first_date = Date.new(year, month, 1)
+  last_date = Date.new(year, month, -1)
   puts "#{month}月 #{year}".center(20)
-  puts week.join(' ')
+  puts  "日 月 火 水 木 金 土"
 
-  (first_day..last_day).each do |day|
-    if day.day == 1 && day.saturday?
-      print day.day.to_s.rjust((first_wday * i) + i) + "\n"
-    elsif day.day == 1
-      print day.day.to_s.rjust((first_wday * i) + i)
-    elsif day.saturday?
-      print day.day.to_s.rjust(i) + "\n"
+  (first_date..last_date).each do |date|
+    if date.day == 1
+      print date.day.to_s.rjust((first_date.wday * width) + width)
     else
-      print day.day.to_s.rjust(i)
+      print date.day.to_s.rjust(width)
+    end
+    if date.saturday?
+      print "\n"
     end
   end
   print "\n"
 end
 
-opt = OptionParser.new
 
-params = ARGV.getopts("y:","m:")
-argument_year = params["y"].to_i
-argument_month = params["m"].to_i
+params = ARGV.getopts("y:", "m:")
+argument_year = params["y"]
+argument_month = params["m"]
 
-if argument_year == 0 && argument_month == 0
-  create_calender
-elsif argument_month == 0
-  create_calender(year: argument_year)
-elsif argument_year == 0
-  create_calender(month: argument_month)
-else
-  create_calender(month: argument_month, year: argument_year)
-end
+month = argument_month ? argument_month.to_i : Date.today.month
+year = argument_year ? argument_year.to_i : Date.today.year
+
+create_calender(month, year)
