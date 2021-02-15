2f4d74f6e62cbc54661895d092f26d2f2482faf0
tatuya4451 <tatuya44511336@gmail.com>
2021-02-15 09:14:16 +0900

    frame_cont10とそれ以外で条件分岐を作る

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -18,27 +18,31 @@ shots.each_with_index do |s, i|
   shots_count += 1
   frame_score += s
 
-  if shots_count == 1 && s == 10 && frame_count == 10
-    total_score += 10 + shots[i + 1] + shots[i + 2]
-    break
-  elsif shots_count == 2 && frame_score == 10 && frame_count == 10
-    total_score += 10 + shots[i + 1]
-    break
-  elsif shots_count == 1 && s == 10
-    total_score += 10 + shots[i + 1] + shots[i + 2]
-    frame_count += 1
-    shots_count = 0
-    frame_score = 0
-  elsif shots_count == 2 && frame_score == 10
-    total_score += 10 + shots[i + 1]
-    frame_count += 1
-    shots_count = 0
-    frame_score = 0
-  elsif shots_count == 2
-    total_score += frame_score
-    frame_count += 1
-    shots_count = 0
-    frame_score = 0
+  if frame_count == 10
+    if shots_count == 1 && s == 10
+      total_score += 10 + shots[i + 1] + shots[i + 2]
+      break
+    elsif shots_count == 2 && frame_score == 10
+      total_score += 10 + shots[i + 1]
+      break
+    end
+  else
+    if shots_count == 1 && s == 10
+      total_score += 10 + shots[i + 1] + shots[i + 2]
+      frame_count += 1
+      shots_count = 0
+      frame_score = 0
+    elsif shots_count == 2 && frame_score == 10
+      total_score += 10 + shots[i + 1]
+      frame_count += 1
+      shots_count = 0
+      frame_score = 0
+    elsif shots_count == 2
+      total_score += frame_score
+      frame_count += 1
+      shots_count = 0
+      frame_score = 0
+    end
   end
 end
 
