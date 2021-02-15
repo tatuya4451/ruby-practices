95c379c370147afdebbf6dcea754ce4f1567c8c3
tatuya4451 <tatuya44511336@gmail.com>
2021-02-15 13:37:52 +0900

    rubocop -A

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -6,7 +6,7 @@ scores = score.chars
 
 shots = []
 scores.each do |s|
-  s == 'X' ? shots << 10 : shots << s.to_i
+  shots << (s == 'X' ? 10 : s.to_i)
 end
 
 total_score = 0
@@ -26,23 +26,21 @@ shots.each_with_index do |s, i|
       total_score += 10 + shots[i + 1]
       break
     end
-  else
-    if shots_count == 1 && s == 10
-      total_score += 10 + shots[i + 1] + shots[i + 2]
-      frame_count += 1
-      shots_count = 0
-      frame_score = 0
-    elsif shots_count == 2 && frame_score == 10
-      total_score += 10 + shots[i + 1]
-      frame_count += 1
-      shots_count = 0
-      frame_score = 0
-    elsif shots_count == 2
-      total_score += frame_score
-      frame_count += 1
-      shots_count = 0
-      frame_score = 0
-    end
+  elsif shots_count == 1 && s == 10
+    total_score += 10 + shots[i + 1] + shots[i + 2]
+    frame_count += 1
+    shots_count = 0
+    frame_score = 0
+  elsif shots_count == 2 && frame_score == 10
+    total_score += 10 + shots[i + 1]
+    frame_count += 1
+    shots_count = 0
+    frame_score = 0
+  elsif shots_count == 2
+    total_score += frame_score
+    frame_count += 1
+    shots_count = 0
+    frame_score = 0
   end
 end
 
