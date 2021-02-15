440e829194bf48c28e442d2c94ed0a6f87cb0947
tatuya4451 <tatuya44511336@gmail.com>
2021-02-15 19:43:47 +0900

    sをshotに変更

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -11,19 +11,19 @@ frame_score = 0
 frame_count = 1
 shots_count = 0
 
-shots.each_with_index do |s, i|
+shots.each_with_index do |shot, i|
   shots_count += 1
-  frame_score += s
+  frame_score += shot
 
   if frame_count == 10
-    if shots_count == 1 && s == 10
+    if shots_count == 1 && shot == 10
       total_score += 10 + shots[i + 1] + shots[i + 2]
       break
     elsif shots_count == 2 && frame_score == 10
       total_score += 10 + shots[i + 1]
       break
     end
-  elsif shots_count == 1 && s == 10
+  elsif shots_count == 1 && shot == 10
     total_score += 10 + shots[i + 1] + shots[i + 2]
     frame_count += 1
     shots_count = 0
