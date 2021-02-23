b124285f26551024f6299fd02360f6b5286a4f26
tatuya4451 <tatuya44511336@gmail.com>
2021-02-23 16:26:15 +0900

    loptionを追加

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'optparse'
+
 def wc(hash)
   hash.each do |file_name, file|
     file.each_value do |value|
@@ -9,8 +11,16 @@ def wc(hash)
   end
 end
 
+def l_option(hash)
+  hash.each do |file_name, file|
+    print file[:lines].to_s.rjust(8)
+    print " #{file_name}\n"
+  end
+end
+
 def file
   files = ARGV
+  argument_l = ARGV.getopts('l')
   hash = Hash.new { |h,k| h[k] = {} }
 
   files.each do |file|
@@ -20,14 +30,22 @@ def file
     hash[file][:bytes] = str.size
   end
 
-  if files.size == 1
-    wc(hash)
-  else
-    wc(hash)
+  if argument_l && files.size == 1
+    l_option(hash)
+  elsif argument_l
+    l_option(hash)
     print hash.values.inject(0) {|sum, hash| sum + hash[:lines]}.to_s.rjust(8)
-    print hash.values.inject(0) {|sum, hash| sum + hash[:words]}.to_s.rjust(8)
-    print hash.values.inject(0) {|sum, hash| sum + hash[:bytes]}.to_s.rjust(8)
     print " total \n"
+  else
+    if files.size == 1
+      wc(hash)
+    else
+      wc(hash)
+      print hash.values.inject(0) {|sum, hash| sum + hash[:lines]}.to_s.rjust(8)
+      print hash.values.inject(0) {|sum, hash| sum + hash[:words]}.to_s.rjust(8)
+      print hash.values.inject(0) {|sum, hash| sum + hash[:bytes]}.to_s.rjust(8)
+      print " total \n"
+    end
   end
 end
 
