485eb8247a488970281a51bb388c382578aa4278
tatuya4451 <tatuya44511336@gmail.com>
2021-02-23 19:40:21 +0900

    メソッドに分割

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'optparse'
 
-def wc(hash)
+def wc_count(hash)
   hash.each do |file_name, file|
     file.each_value do |value|
       print value.to_s.rjust(8)
@@ -11,17 +12,38 @@ def wc(hash)
   end
 end
 
-def l_option(hash)
+def wc_normal(hash, files)
+  if files.size == 1
+    wc_count(hash)
+  else
+    wc_count(hash)
+    print hash.values.inject(0) { |sum, hash| sum + hash[:lines] }.to_s.rjust(8)
+    print hash.values.inject(0) { |sum, hash| sum + hash[:words] }.to_s.rjust(8)
+    print hash.values.inject(0) { |sum, hash| sum + hash[:bytes] }.to_s.rjust(8)
+    print " total \n"
+    end
+  end
+
+def line_count_only(hash)
   hash.each do |file_name, file|
     print file[:lines].to_s.rjust(8)
     print " #{file_name}\n"
   end
 end
 
-def file
-  files = ARGV
+def l_option(hash, files)
+  if files.size == 1
+    line_count_only(hash)
+  else
+    line_count_only(hash)
+      print hash.values.inject(0) { |sum, hash| sum + hash[:lines] }.to_s.rjust(8)
+      print " total \n"
+  end
+end
+
+def wc(files)
   argument_l = ARGV.getopts('l')
-  hash = Hash.new { |h,k| h[k] = {} }
+  hash = Hash.new { |h, k| h[k] = {} }
 
   files.each do |file|
     str = File.read(file)
@@ -30,31 +52,34 @@ def file
     hash[file][:bytes] = str.size
   end
 
-  if argument_l && files.size == 1
-    l_option(hash)
-  elsif argument_l
-    l_option(hash)
-    print hash.values.inject(0) {|sum, hash| sum + hash[:lines]}.to_s.rjust(8)
-    print " total \n"
+  if argument_l
+    l_option(hash, files)
   else
-    if files.size == 1
-      wc(hash)
-    else
-      wc(hash)
-      print hash.values.inject(0) {|sum, hash| sum + hash[:lines]}.to_s.rjust(8)
-      print hash.values.inject(0) {|sum, hash| sum + hash[:words]}.to_s.rjust(8)
-      print hash.values.inject(0) {|sum, hash| sum + hash[:bytes]}.to_s.rjust(8)
-      print " total \n"
-    end
+    wc_normal(hash, files)
   end
 end
 
-file
+def output_with_out_stdin
+  files = ARGV
+  if files.empty?
+    # noop
+  else
+    wc(files)
+  end
+end
 
-pipe = []
-input = $stdin.read
-pipe << input.count("\n")
-pipe << input.split(/\s+/).size
-pipe << input.size
-pipe.each { |file| print file.to_s.rjust(8) }
-print "\n"
+def output_with_stdin
+  pipe = []
+  input = $stdin.read
+  pipe << input.count("\n")
+  pipe << input.split(/\s+/).size
+  pipe << input.size
+  pipe.each { |file| print file.to_s.rjust(8) }
+  print "\n"
+end
+
+if $stdin.tty?
+  output_with_out_stdin
+else
+  output_with_stdin
+end
